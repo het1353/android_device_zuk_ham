c4cbfe1ebd399e8b3f9cc2e78bd70e3f01c58986
ShubhamB <shubhamprince111@gmail.com>
2018-07-13 05:35:48 +0000

    ham: gps: remove current gps implementation
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- ham.mk
+++ ham.mk
@@ -117,17 +117,6 @@ PRODUCT_PACKAGES += \
     memtrack.msm8974 \
     liboverlay
 
-# GPS
-PRODUCT_PACKAGES += \
-    gps.msm8974
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
-#    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8974
