29b821048d1a8c87501a1e6a2a5a2985569cfaed
ShubhamB <shubhamprince111@gmail.com>
2018-08-12 10:41:42 +0000

    ham: dont build legacy camera impl
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -36,9 +36,9 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service.ham \
-    camera.device@1.0-impl-legacy
+    camera.device@1.0-impl
 
 # DRM
 PRODUCT_PACKAGES += \
